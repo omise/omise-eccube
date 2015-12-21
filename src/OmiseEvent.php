520f873b50edf0af01022099c525f361850c82bf
AD-PC72MAC <naoki@alpha-do.com>
2015-12-21 12:43:36 +0900

    カート画面途中。次回OmiseEvent.php TODO参照

--- src/OmiseEvent.php
+++ src/OmiseEvent.php
@@ -1,7 +1,8 @@
 <?php
 namespace Plugin\OmisePaymentGateway;
 
-use Eccube\Common\Constant;
+use Eccube\Event\RenderEvent;
+use Eccube\Event\ShoppingEvent;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
 /**
@@ -13,5 +14,26 @@ class OmiseEvent {
 
 	public function __construct($app) {
 		$this->app = $app;
+	}
+	
+	public function onRenderShoppingBefore(FilterResponseEvent $event) {
+		$request = $event->getRequest();
+		$response = $event->getResponse();
+		$order = $this->app['eccube.repository.order']->findOneBy(array('pre_order_id' => $this->app['eccube.service.cart']->getPreOrderId()));
+		$id = $order->getPayment()->getId();
+
+		$omiseConfigService = $this->app['eccube.plugin.service.omise_pg_config'];
+		
+		// TODO 決済画面でクレジット選択状態での入力表示〜決済投入
+		return;
+		
+	}
+	
+	public function onRenderShoppingConfirmBefore(FilterResponseEvent $event) {
+		
+	}
+	
+	public function onControllerShoppingConfirmBefore() {
+		
 	}
 }
