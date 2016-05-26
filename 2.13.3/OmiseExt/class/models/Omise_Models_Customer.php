5204865b99894bc47b6c38e0c3103fdd8160ec25
akinrt <aki.d.sc@gmail.com>
2016-05-26 14:45:47 +0700

    modify 2.13.3/OmiseExt/class/models/Omise_Models_Customer.php and delete submodule

--- 2.13.3/OmiseExt/class/models/Omise_Models_Customer.php
+++ 2.13.3/OmiseExt/class/models/Omise_Models_Customer.php
@@ -12,7 +12,8 @@
  *
  * @author Akira / Omise
  */
-class Omise_Models_Customer {
+class Omise_Models_Customer
+{
 
   /**
    * @var $customer_id 0 or NULL = user not logged in
@@ -20,16 +21,18 @@ class Omise_Models_Customer {
   private $customer_id;
   private $objOmiseCustomer;
 
-  public function __construct($customer_id) {
-    $this->customer_id = intval($customer_id);
+  public function __construct($customer_id)
+  {
+      $this->customer_id = intval($customer_id);
   }
 
   /**
    * Return the customer_id as int type
    * @return [type] [description]
    */
-  public function getCustomerId() {
-    return $this->customer_id;
+  public function getCustomerId()
+  {
+      return $this->customer_id;
   }
 
   /**
@@ -37,21 +40,22 @@ class Omise_Models_Customer {
    *
    * @return OmiseCard|null the OmiseCard instance or null
    */
-  public function fetchSavedCardForCustomer() {
-    if ($this->lfIsNoCustomer()) {
-      return null;
-    }
-    $objCustomer = $this->lfFetchOmiseCustomer();
-    if ($objCustomer !== null) {
-      $card = OmiseWrapper::customerDefaultCard($this->loadOmiseCustomerId());
-      if($card !== null) {
-        return $card['object'] === 'card' ? $card : null;
-      } else {
-        return null;
+  public function fetchSavedCardForCustomer()
+  {
+      if ($this->lfIsNoCustomer()) {
+          return null;
       }
-    } else {
-      return null;
-    }
+      $objCustomer = $this->lfFetchOmiseCustomer();
+      if ($objCustomer !== null) {
+          $card = OmiseWrapper::customerDefaultCard($this->loadOmiseCustomerId());
+          if($card !== null) {
+              return $card['object'] === 'card' ? $card : null;
+          } else {
+              return null;
+          }
+      } else {
+          return null;
+      }
   }
 
   /**
@@ -60,36 +64,38 @@ class Omise_Models_Customer {
    * @param  string $token Omise token id
    * @return string Omise customer id
    */
-  public function saveCardForCustomer($token) {
-    if ($this->lfIsNoCustomer()) {
-      return null;
-    }
-    // load omise customer id
-    $omise_id = $this->loadOmiseCustomerId();
+  public function saveCardForCustomer($token)
+  {
+      if ($this->lfIsNoCustomer()) {
+          return null;
+      }
+      // load omise customer id
+      $omise_id = $this->loadOmiseCustomerId();
 
-    // set attr for updation
-    $arrAttributes = array('card' => $token);
-    if (isset($_SESSION['customer']['email'])) {
-      $arrAttributes['email'] = $_SESSION['customer']['email'];
-    }
+      // set attr for updation
+      $arrAttributes = array('card' => $token);
+      if (isset($_SESSION['customer']['email'])) {
+          $arrAttributes['email'] = $_SESSION['customer']['email'];
+      }
 
-    // Delete old card, because we just allowed to store 1 card
-    OmiseWrapper::destoryAllCustomerCard($omise_id);
+      // Delete old card, because we just allowed to store 1 card
+      OmiseWrapper::destoryAllCustomerCard($omise_id);
 
-    $omise_id = $this->lfUpdateOmiseCustomer($omise_id, $arrAttributes);
-    $this->lfInsertOmiseCustomerId($omise_id);
-    return $omise_id;
+      $omise_id = $this->lfUpdateOmiseCustomer($omise_id, $arrAttributes);
+      $this->lfInsertOmiseCustomerId($omise_id);
+      return $omise_id;
   }
 
   /**
    * Delete the default card on the Omise Customer
    * @return void
    */
-  public function deleteDefaultCard() {
-    if ($this->lfIsNoCustomer()) {
-      return;
-    }
-    OmiseWrapper::customerDeleteDefaultCard($this->loadOmiseCustomerId());
+  public function deleteDefaultCard()
+  {
+      if ($this->lfIsNoCustomer()) {
+          return;
+      }
+      OmiseWrapper::customerDeleteDefaultCard($this->loadOmiseCustomerId());
   }
 
   /**
@@ -98,28 +104,30 @@ class Omise_Models_Customer {
    * @return boolean
    */
 
-  private function lfIsNoCustomer() {
-    return $this->customer_id === 0;
+  private function lfIsNoCustomer()
+  {
+      return $this->customer_id === 0;
   }
 
   /**
    * Fetch Omise Customer
    * @return OmiseCustomer  the OmiseCustomer instance
    */
-  private function lfFetchOmiseCustomer() {
-    if ($this->objOmiseCustomer !== null) {
-      return $this->objOmiseCustomer;
-    }
-    $omise_customer_id = $this->loadOmiseCustomerId();
-    if ($omise_customer_id === null)
-      return null;
-    try {
-      $this->objOmiseCustomer = OmiseWrapper::customerRetrieve($omise_customer_id);
-    } catch (OmiseException $e) {
-      $this->lfRemoveOmiseCustomerId();
-    }
+  private function lfFetchOmiseCustomer()
+  {
+      if ($this->objOmiseCustomer !== null) {
+          return $this->objOmiseCustomer;
+      }
+      $omise_customer_id = $this->loadOmiseCustomerId();
+      if ($omise_customer_id === null)
+          return null;
+      try {
+          $this->objOmiseCustomer = OmiseWrapper::customerRetrieve($omise_customer_id);
+      } catch (OmiseException $e) {
+          $this->lfRemoveOmiseCustomerId();
+      }
 
-    return $this->objOmiseCustomer;
+      return $this->objOmiseCustomer;
   }
 
   /**
@@ -129,25 +137,24 @@ class Omise_Models_Customer {
    * @return string  The Omise Customer id
    */
 
-  private function lfUpdateOmiseCustomer($omise_id, $arrAttributes) {
-    $updated = false;
-    echo $omise_id ;
-    print_r($arrAttributes);
-    // Update Omise Customer by customer_id
-    if ($omise_id !== null) {
-      try {
-        $this->objOmiseCustomer = OmiseWrapper::customerUpdate($omise_id, $arrAttributes);
-        $updated = true;
-      } catch (OmiseException $e) {
-        throw $e;
+  private function lfUpdateOmiseCustomer($omise_id, $arrAttributes)
+  {
+      $updated = false;
+      // Update Omise Customer by customer_id
+      if ($omise_id !== null) {
+          try {
+              $this->objOmiseCustomer = OmiseWrapper::customerUpdate($omise_id, $arrAttributes);
+              $updated = true;
+          } catch (OmiseException $e) {
+              throw $e;
+          }
       }
-    }
-    // Create new Omise Customer
-    if (!$updated) {
-      $this->objOmiseCustomer = OmiseWrapper::customerCreate($arrAttributes);
-    }
+      // Create new Omise Customer
+      if (!$updated) {
+          $this->objOmiseCustomer = OmiseWrapper::customerCreate($arrAttributes);
+      }
 
-    return $this->objOmiseCustomer['id'];
+      return $this->objOmiseCustomer['id'];
   }
 
   /**
@@ -155,45 +162,48 @@ class Omise_Models_Customer {
    * @param  string  $omise_id  The Omise Customer id
    * @return void
    */
-  private function lfInsertOmiseCustomerId($omise_id) {
-    $objQuery = SC_Query_Ex::getSingletonInstance();
-    $objQuery->begin();
-    $arrMapping = self::lfLoadMapping($objQuery);
-    $arrMapping[$this->customer_id] = $omise_id;
-    self::lfSaveMapping($objQuery, $arrMapping);
-    $objQuery->commit();
+  private function lfInsertOmiseCustomerId($omise_id)
+  {
+      $objQuery = SC_Query_Ex::getSingletonInstance();
+      $objQuery->begin();
+      $arrMapping = self::lfLoadMapping($objQuery);
+      $arrMapping[$this->customer_id] = $omise_id;
+      self::lfSaveMapping($objQuery, $arrMapping);
+      $objQuery->commit();
   }
 
   /**
    * Load Omise Customer id from dtb_payment.memo04
    * @return string|null return id string or null (if there was no record for the eccube customer)
    */
-  public function loadOmiseCustomerId() {
-    if ($this->lfIsNoCustomer()) {
-      return null;
-    }
-    $objQuery = SC_Query_Ex::getSingletonInstance();
-    $arrMapping = self::lfLoadMapping($objQuery);
-    if (!array_key_exists($this->customer_id, $arrMapping)) {
-      return null;
-    }
+  public function loadOmiseCustomerId()
+  {
+      if ($this->lfIsNoCustomer()) {
+          return null;
+      }
+      $objQuery = SC_Query_Ex::getSingletonInstance();
+      $arrMapping = self::lfLoadMapping($objQuery);
+      if (!array_key_exists($this->customer_id, $arrMapping)) {
+          return null;
+      }
 
-    return $arrMapping[$this->customer_id];
+      return $arrMapping[$this->customer_id];
   }
 
   /**
    * Remove Omise Customer from dtb_payment.memo04
    * @return void
    */
-  private function lfRemoveOmiseCustomerId() {
-    $objQuery = SC_Query_Ex::getSingletonInstance();
-    $objQuery->begin();
-    $arrMapping = self::lfLoadMapping($objQuery);
-    if (array_key_exists($this->customer_id, $arrMapping)) {
-      unset($arrMapping[$this->customer_id]);
-      self::lfSaveMapping($objQuery, $arrMapping);
-    }
-    $objQuery->commit();
+  private function lfRemoveOmiseCustomerId()
+  {
+      $objQuery = SC_Query_Ex::getSingletonInstance();
+      $objQuery->begin();
+      $arrMapping = self::lfLoadMapping($objQuery);
+      if (array_key_exists($this->customer_id, $arrMapping)) {
+          unset($arrMapping[$this->customer_id]);
+          self::lfSaveMapping($objQuery, $arrMapping);
+      }
+      $objQuery->commit();
   }
 
   /**
@@ -207,10 +217,11 @@ class Omise_Models_Customer {
    *           1: "Omise Customer Id"
    *         )
    */
-  private static function lfLoadMapping($objQuery) {
-    $s_data = $objQuery->getCol(OMISE_MDL_CUSTOMER_DATA_COL, 'dtb_payment', 'module_id = ?',  array(OMISE_MDL_ID));
-    $arrMapping = unserialize($s_data[0]);
-    return is_array($arrMapping) ? $arrMapping : array();
+  private static function lfLoadMapping($objQuery)
+  {
+      $s_data = $objQuery->getCol(OMISE_MDL_CUSTOMER_DATA_COL, 'dtb_payment', 'module_id = ?',  array(OMISE_MDL_ID));
+      $arrMapping = unserialize($s_data[0]);
+      return is_array($arrMapping) ? $arrMapping : array();
   }
 
   /**
@@ -219,9 +230,10 @@ class Omise_Models_Customer {
    * @param  array        $arrMapping the Omise Customer mapping array
    * @return void
    */
-  private static function lfSaveMapping($objQuery, $arrMapping) {
-    unset($arrMapping[0]); // id > 0 なので 0 は必ず排除
-    $arrNewVal = array(OMISE_MDL_CUSTOMER_DATA_COL => serialize($arrMapping));
-    $objQuery->update('dtb_payment', $arrNewVal, 'module_id = ?',  array(OMISE_MDL_ID));
+  private static function lfSaveMapping($objQuery, $arrMapping)
+  {
+      unset($arrMapping[0]); // id > 0 なので 0 は必ず排除
+      $arrNewVal = array(OMISE_MDL_CUSTOMER_DATA_COL => serialize($arrMapping));
+      $objQuery->update('dtb_payment', $arrNewVal, 'module_id = ?',  array(OMISE_MDL_ID));
   }
 }
