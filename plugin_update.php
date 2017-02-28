823c187504ec2ddec3d6d7b64b70720d5f726d69
guzzilar <iam.nuttanon@gmail.com>
2017-02-28 18:50:40 +0700

    plugin_update.php:: copy needed files from tmp to plugin dir, rebuild release file.

--- plugin_update.php
+++ plugin_update.php
@@ -9,5 +9,31 @@ class plugin_update
      */
     public function update(array $plugin_info, SC_Plugin_Installer $installer)
     {
+        copy(
+            DOWNLOADS_TEMP_PLUGIN_UPDATE_DIR . "class/models/Omise_Models_Charge.php",
+            PLUGIN_UPLOAD_REALDIR . $plugin_info['plugin_code'] . "/class/models/Omise_Models_Charge.php"
+        );
+
+        copy(
+            DOWNLOADS_TEMP_PLUGIN_UPDATE_DIR . "inc/include.php",
+            PLUGIN_UPLOAD_REALDIR . $plugin_info['plugin_code'] . "/inc/include.php"
+        );
+
+        copy(
+            DOWNLOADS_TEMP_PLUGIN_UPDATE_DIR . "plugin_info.php",
+            PLUGIN_UPLOAD_REALDIR . $plugin_info['plugin_code'] . "/plugin_info.php"
+        );
+
+        copy(
+            DOWNLOADS_TEMP_PLUGIN_UPDATE_DIR . "plugin_update.php",
+            PLUGIN_UPLOAD_REALDIR . $plugin_info['plugin_code'] . "/plugin_update.php"
+        );
+
+        copy(
+            DOWNLOADS_TEMP_PLUGIN_UPDATE_DIR . "logo.png",
+            PLUGIN_UPLOAD_REALDIR . $plugin_info['plugin_code'] . "/logo.png"
+        );
+
+        $installer->copyFile('logo.png', 'logo.png');
     }
 }
