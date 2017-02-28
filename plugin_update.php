600d51a8b254abae76a827424b32963c74c2ce33
guzzilar <iam.nuttanon@gmail.com>
2017-02-28 16:35:25 +0700

    Add 'plugin_update.php', the file that EC-Cube system requires for updating plugin.

---
+++ plugin_update.php
@@ -0,0 +1,13 @@
+<?php
+class plugin_update
+{
+    /**
+     * @param  array               $plugin_info プラグイン情報
+     * @param  SC_Plugin_Installer $installer   プラグインインストーラー
+     *
+     * @return void
+     */
+    public function update(array $plugin_info, SC_Plugin_Installer $installer)
+    {
+    }
+}
